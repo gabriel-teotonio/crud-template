40717a3643f299b7cc68fe8eaba329007cab8c8b
Gabriel silva <gabriel23teotonio@gmail.com>
2023-09-06 08:19:20 -0300

    feat: validate form with yup

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,14 +1,26 @@
 import { useForm } from "react-hook-form";
 import "./styles.css";
 import { api } from "../../lib/axio";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useNavigate } from "react-router-dom";
+
+const postSchema = yup.object({
+  title: yup.string().required("preecha o campo título"),
+  description: yup.string().required("preencha o campo descrição"),
+  content: yup.string().required("preencha o campo conteudo"),
+})
 
 export function Form({ title, textButton }) {
-
-  const { register, handleSubmit } = useForm()
+  const navigate = useNavigate()
+  const { register, handleSubmit, formState:{errors} } = useForm({
+    resolver: yupResolver(postSchema)
+  })
 
   const handleCreatePost = (data) => {
     api.post("/posts", data)
-    console.log(data)
+    navigate("/")
+    console.log("criado com sucesso!")
   }
 
   return (
@@ -16,14 +28,17 @@ export function Form({ title, textButton }) {
       <h2>{title}</h2>
       <div className="field">
         <input placeholder="Título" {...register("title")}/>
+        {errors.title?.message}
       </div>
 
       <div className="field">
         <input placeholder="Descrição" {...register("description")}/>
+        {errors.description?.message}
       </div>
 
       <div className="field">
         <textarea placeholder="Conteúdo" {...register("content")}/>
+        {errors.content?.message}
       </div>
 
       <button type="submit">{textButton}</button>
